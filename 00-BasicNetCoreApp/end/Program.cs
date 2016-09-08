a098e9e6fb56a0eabd1a5c1cbea6c72f0537e01a
Winslow VanDevanter <wynvandev@Winslows-MacBook-Pro.local>
2016-09-08 12:48:31 -0400

    end product for basic core app

---
+++ 00-BasicNetCoreApp/end/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Run(context => context.Response.WriteAsync("Hello World, from ASP.NET!")))
+                .Build();
+
+            host.Run();
+        }
+    }
+}
