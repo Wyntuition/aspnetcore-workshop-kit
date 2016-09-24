ee702548a125ad7f40614cf736c81acfa89bb21f
Winslow VanDevanter <wynvandev@Winslows-MacBook-Pro.local>
2016-09-24 08:45:42 -0400

    typos

--- 00-BasicAspNetCoreApp/end/Startup.cs
+++ 00-BasicAspNetCoreApp/end/Startup.cs
@@ -6,8 +6,8 @@ namespace ConsoleApplication
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        //public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. 
+        // Middleware is configured here.
         public void Configure(IApplicationBuilder app)
         {
             app.Run(context => context.Response.WriteAsync("Hello World, from ASP.NET!"));
